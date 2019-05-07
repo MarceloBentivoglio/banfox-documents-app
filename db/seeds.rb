8c9b7acea27c23118531f2ff5a3c34ec78211aba
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2019-05-07 13:04:25 -0300

    changed the layout of the document. Now the seller can see the protection and the first deposite

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,16 @@ ActiveRecord::Base.transaction do
   user1 = User.create!(
     email: "joaquim@banfox.com.br",
     password: "123123",
+    authentication_token: "dx3zDP3cY4FXs8qV12W3",
     admin: true,
   )
 
   variable_content1 = {
     seller: {
-      company_name: "BIORT COMÉRCIO DE PROD. MÉDICOS E CIRÚRGICOS LTDA.",
+      company_name: "biort comercio de prod. medicos e cirúrgicos ltda.",
       cnpj: "08.588.244/0001-49",
-      address: "RUA DOS MENINOS, 431 - NOVA GERTY",
-      city: "São Caetano do Sul",
+      address: "rua dos meninos, 431 - nova gerty",
+      city: "são caetano do sul",
       state: "SP",
       zip_code: "09580-300",
       email: "financeiro1@biortimplantes.com.br",
@@ -22,57 +23,55 @@ ActiveRecord::Base.transaction do
       id: "1",
       time: "2019-04-29T10:30:00-03:00",
       gross_value: "16.905,33",
-      fator: "1.071,73",
-      advalorem: "20,84",
-      iof: "20,65",
-      iof_adicional: "60,17",
       fee: "62,41",
       net_value: "15.669,53",
+      first_deposit: "13.524,26",
+      protection: "3.381,06"
     },
     installments: [
       {
-        payer_company_name: "BELTI COMERCIO DE PRODUTOS MEDICOS",
+        payer_company_name: "belti comercio de produtos medicos",
         registration: "17.797.083/0001-94",
-        type: "DMR",
-        invoice_number: "6609",
+        type: "dmr",
         number: "006.609/01",
         due_date: "17/08/2018",
         outstanding_days: "27",
         value: "5.274,00",
-        fator_advalorem: "246,82",
-        iof: "23,66",
-        net_value: "5.003,52"
+        fee: "246,82",
+        net_value: "5.003,52",
+        first_deposit: "4.000,26",
+        protection: "1.003,00"
       },
       {
-        payer_company_name: "ALLIANZ SAUDE SA",
+        payer_company_name: "allianz saude sa",
         registration: "04.439.627/0001-02",
-        type: "DMR",
-        invoice_number: "6632",
+        type: "dmr",
         number: "006.632/01",
         due_date: "23/08/2018",
         outstanding_days: "33",
         value: "5.450,91",
-        fator_advalorem: "312,56",
-        iof: "25,45",
+        fee: "312,56",
         net_value: "5.112,90",
+        first_deposit: "4.000,26",
+        protection: "1.003,00"
       },
       {
-        payer_company_name: "HOSPITAL SANTA PAULA",
+        payer_company_name: "hospital santa paula",
         registration: "60.777.901/0001-16",
-        type: "DMR",
-        invoice_number: "6638",
+        type: "dmr",
         number: "006.638/01",
         due_date: "08/09/2018",
         outstanding_days: "49",
         value: "6.180,42",
-        fator_advalorem: "533,19",
-        iof: "31,70",
-        net_value: "5.615,53"
+        fee: "533,19",
+        net_value: "5.615,53",
+        first_deposit: "4.000,26",
+        protection: "1.003,00"
       }
     ],
     signers: [
       {
-        name: "Joaquim Primeiro Devedor",
+        name: "joaquim primeiro devedor",
         birthdate: "1989-04-21",
         mobile: "11998308090",
         documentation: "096.701.336-45",
@@ -80,12 +79,17 @@ ActiveRecord::Base.transaction do
         sign_as: ["sign", "joint_debtor"]
       }
     ]
-
   }
 
   document1 = Document.create!(
     user: user1,
     variable_content: variable_content1,
   )
+
+  puts "-----------------\n\n"
+  puts "Seed created:"
+  puts "User: #{user1.email}"
+  puts "Password: #{user1.password}"
+  puts "\n\n-----------------"
 end
 
