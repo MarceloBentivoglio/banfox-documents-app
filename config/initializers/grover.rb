672e58646c3209a7a6812950d3f968be78130207
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2019-05-14 18:21:14 -0300

    increased the pdf style

---
+++ config/initializers/grover.rb
@@ -0,0 +1,15 @@
+Grover.configure do |config|
+  config.options = {
+    # format: 'A4',
+    margin: {
+      top: '50px',
+      right: "0px",
+      bottom: '50px',
+      left: "0px"
+    },
+    # prefer_css_page_size: true,
+    # emulate_media: 'screen',
+    # cache: false,
+    # timeout: 0 # Timeout in ms. A value of `0` means 'no timeout'
+  }
+end
